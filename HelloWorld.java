cb6ccc536ea5dbf6c5f95718870afd3dea6b570b
teja123143 <mula.ravitejareddy@gmail.com>
2019-12-13 16:39:01 +0530

    responed to with safety message

--- HelloWorld.java
+++ HelloWorld.java
@@ -10,5 +10,6 @@ public class HelloWorld {
     System.out.println("Hello, Banglore good morning");
     System.out.println("Hello, Marathahalli");
     System.out.println("Hello, SGR dental college road");
+    System.out.println("Hello, Reached to origin");
   }
 }
